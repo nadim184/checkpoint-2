45d6071db826a3d01caf337ff879d1dbff0935a2
nadim184 <51932411+nadim184@users.noreply.github.com>
2019-07-16 10:45:14 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import photo from './logo.svg';
+import './App.css';
+import algorythme from './azerty.png'
+
+function App() {
+  return (
+   <div className="App">
+      
+      <header className="App-header">
+<div className="st" >
+	<h1 className="title red"> My title </h1>
+
+	
+
+<img src={algorythme} />
+
+
+
+	<img src={photo} />
+  </div>
+
+</header>
+
+
+</div>
+      
+      
+    
+
+  );
+}
+
+export default App;
